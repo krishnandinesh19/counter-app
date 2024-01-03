e2a9ac81a60136dab6a3a150f8085cd1b15718ec
dinesh krishnan <dkkrishnan20@gmail.com>
2024-01-03 09:14:02 +0400

    submit

---
+++ src/xcounter.js
@@ -0,0 +1,25 @@
+// src/Counter.js
+import React, { useState } from 'react';
+import './xcounter.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
